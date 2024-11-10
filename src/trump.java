fb50237ba40545f0e3a87fa262ac96185c2d9c50
Subin <subinsb135@gmail.com>
2024-11-10 17:41:24 +0530

    trump china

--- src/trump.java
+++ src/trump.java
@@ -3,5 +3,6 @@ public static void main (String []args){
 System.out.println("Hi i'm trump");
 System.out.println("Hi i'm putin");
 System.out.println("Hi i'm modi");
+System.out.println("Put tariffs on china");
     }
 }
